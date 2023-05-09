bb2e846e16312d09a4624eefde1bfae0f2edf2c7
Blanka Pałucka <blankapalucka@gmail.com>
2023-05-09 16:32:42 +0200

    background picture added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 
 import './App.css';
 import Form from './Form';
+import background from "./img/background.jpg";
 
 function App() {
 
   return (
     <body className="body element" >
-
-      <Form/>
-
-</body >
+      <div style={{ backgroundImage: `url("${background}")` }}>
+        <Form />
+      </div>
+    </body >
   );
 };
+
 
 
 
