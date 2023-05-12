1e0d78d332645d171ce160f49026991da9962ae8
Blanka Pałucka <blankapalucka@gmail.com>
2023-05-12 13:51:04 +0200

    adding currencies

---
+++ src/Form/currencies.js
@@ -0,0 +1,23 @@
+export const currencies =[
+    {
+        short: "EUR",
+        name: "euro",
+        rate: 4.7018,
+    },
+    {
+        short: "USD",
+        name: "dolar amerykański",
+        rate: 4.4626,
+    },
+    {
+        short: "GBO",
+        name: "funt brytyjski",
+        rate: 5.2811,
+    },
+    {
+        short: "NOK",
+        name: "korona norweska",
+        rate: 0.4172,
+    },
+];
+
