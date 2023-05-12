9ed7b12d9aaff7879137036725f8b88e095ed2f7
Blanka Pałucka <blankapalucka@gmail.com>
2023-05-12 13:52:39 +0200

    adding target amount to result

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,11 +1,15 @@
+import "./style.css";
+
 export const Result = ({ result }) => (
 
-<p className="result">
-    {result !== undefined && (
-        <>
-        {result.sourceAmount.toFixed(2)}
-        </>
-    )}
-</p>    
+    <p className="result">
+        {result !== undefined && (
+            <>
+                {result.sourceAmount.toFixed(2)}
+                {" PLN = "}
+                {result.targetAmount.toFixed(2)} {result.currency}
+            </>
+        )}
+    </p>
 );
 
